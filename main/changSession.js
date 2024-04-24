25b5cd13a2ece71e9eabfff9fcfc02aa90047f20
Nguyễn Văn Thuận <jumrk03@gmail.com>
2024-04-24 13:11:36 +0700

    add

--- main/changSession.js
+++
@@ -1,9 +0,0 @@
-export function startSession(userId) {
-    sessionStorage.setItem('userId', userId)
-}
-export function getSession(){
-    return sessionStorage.getItem('userId')
-}
-export function endSession() {
-    sessionStorage.removeItem('userId');
-}
